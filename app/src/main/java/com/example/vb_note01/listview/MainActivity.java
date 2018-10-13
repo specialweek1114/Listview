a3d832cfa04a8af0f66f2e63b310aca5062981dc
vb-maekawa <maekawa@view-bridge.co.jp>
2018-10-13 18:06:26 +0900

    ゲーム一旦完成　表示調整が必要

--- app/src/main/java/com/example/vb_note01/listview/MainActivity.java
+++ app/src/main/java/com/example/vb_note01/listview/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.vb_note01.listview;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -22,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ConstraintLayout dummy_page1 = findViewById(R.id.dummy_page1);
+        final ConstraintLayout dummy_page2 = findViewById(R.id.dummy_page2);
+        dummy_page1.setVisibility(View.INVISIBLE);
+        dummy_page2.setVisibility(View.INVISIBLE);
+
+        final TextView resultScore = this.findViewById(R.id.result_score);
+        final TextView result_message = this.findViewById(R.id.result_message);
+
         List<ListViewData> hiraganaList = new ArrayList<>();
         for (int i = 1; i <= 100; i++) {
             hiraganaList.add(new ListViewData(""+i+"", ""+i+""));
@@ -30,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             hiraganaList.add(new ListViewData("dead", "dead"));
         }
         // リストビューをとってくる
-        ListView listView = this.findViewById(R.id.list_view);
+        final ListView listView = this.findViewById(R.id.list_view);
 
         // アダプター経由でListViewに　あいうえお　をいれる
         ListAdapter ListViewAdapter = new ArrayAdapter<ListViewData>(
@@ -62,46 +75,68 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        final TextView topText = this.findViewById(R.id.top_text);
-
         // アダプタをセット
         listView.setAdapter(ListViewAdapter);
-
-        // CLICKイベントを設定
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                ListView listView = (ListView) adapterView;
-//                ListViewData hiragana = (ListViewData) listView.getItemAtPosition(i);
-//                topText.setText(hiragana.getLeftText());
-//
-//            }
-//        });
 
         listView.setOnScrollListener(new AbsListView.OnScrollListener() {
             int firstItem;
 
+            @SuppressLint("ClickableViewAccessibility")
             @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            public void onScrollStateChanged(AbsListView view, final int scrollState) {
                 if (scrollState == 0) {
+                    resultScore.setText(""+this.firstItem+"point");
                     if (this.firstItem <= 50) {
-                        topText.setText("" + this.firstItem + "point チキン野郎！");
+                        result_message.setText("チキン野郎！");
+                    }else if( this.firstItem > 50 && this.firstItem <= 75 ) {
+                        result_message.setText("なかなか ");
+                    }else if( this.firstItem > 75 && this.firstItem <= 90 ) {
+                        result_message.setText("勇気あり ");
+                    }else if( this.firstItem > 90 && this.firstItem <= 99 ) {
+                        result_message.setText("真の勇者 ");
+                    }else if( this.firstItem == 100 ) {
+                        result_message.setText("パーフェクト！");
+                    }else if( this.firstItem > 100 ){
+                        result_message.setText("死にました");
                     }
-                    if( this.firstItem > 50 && this.firstItem <= 75 ) {
-                        topText.setText("" + this.firstItem + "point なかなか ");
-                    }
-                    if( this.firstItem > 75 && this.firstItem <= 90 ) {
-                        topText.setText("" + this.firstItem + "point 勇気あり ");
-                    }
-                    if( this.firstItem > 90 && this.firstItem <= 99 ) {
-                        topText.setText("" + this.firstItem + "point 真の勇者 ");
-                    }
-                    if( this.firstItem == 100 ) {
-                        topText.setText("" + this.firstItem + "point パーフェクト！");
-                    }
-                    if( this.firstItem > 100 ){
-                        topText.setText("死にました");
-                    }
+
+
+                    listView.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            if (scrollState != 0) {
+                                return false;
+                            } else {
+                                /** タッチイベントをHandlingしたよ！　てことだとおもいます。*/
+                                /** falseを返すと、スクロールするようになります。*/
+                                return true;
+                            }
+                        }
+                    });
+
+                    // ボタンをアクティブにする
+                    dummy_page1.setVisibility(View.VISIBLE);
+                    dummy_page2.setVisibility(View.VISIBLE);
+
+                    //　ボタンクリックでゲーム再スタート
+                    Button restart = findViewById(R.id.restart_button);
+                    restart.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // ボタンを非アクティブにする
+                            dummy_page1.setVisibility(View.INVISIBLE);
+                            dummy_page2.setVisibility(View.INVISIBLE);
+
+                            //リストビューを先頭に移動
+                            listView.setSelection(0);
+                            listView.setOnTouchListener(new View.OnTouchListener() {
+                                @Override
+                                public boolean onTouch(View v, MotionEvent event) {
+                                    return false;
+                                }
+                            });
+                        }
+                    });
                 }
             }
 
